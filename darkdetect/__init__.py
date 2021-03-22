3ab67b1f3e01e90cf364ddc5243a1150b3f84329
Alberto Sottile <alby128@gmail.com>
2021-03-22 22:36:31 +0100

    Bump version to 0.3.0

--- darkdetect/__init__.py
+++ darkdetect/__init__.py
@@ -4,7 +4,7 @@
 #  Distributed under the terms of the 3-clause BSD License.
 #-----------------------------------------------------------------------------
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 import sys
 import platform
