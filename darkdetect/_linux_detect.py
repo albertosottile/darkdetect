18b570b719f09623583a1c4a24a2f4dd03962d5a
Het Daftary <51186433+HetDaftary@users.noreply.github.com>
2021-04-04 16:12:52 +0530

    Previous code did some problems wih Ubuntu
    This one is a better way as it just picks up the current theme in use directly from the gnome gsettings

--- darkdetect/_linux_detect.py
+++ darkdetect/_linux_detect.py
@@ -5,25 +5,21 @@
 #-----------------------------------------------------------------------------
 
 from ctypes import util, cdll, c_char_p, byref
-
+import subprocess
 
 def theme():
     # Here we just triage to GTK settings for now
-    try:
-        gtklib = cdll.LoadLibrary(util.find_library('gtk-3'))
-        gtklib.gtk_init(None, None)
-        settings = gtklib.gtk_settings_get_default()
-        res = c_char_p()
-        gtklib.g_object_get(settings, b"gtk-theme-name", byref(res), 0)
-        theme = res.value.decode()
-    except Exception:
-        return 'Light'
+    out = subprocess.Popen(['gsettings', 'get', 'org.gnome.desktop.interface', 'gtk-theme'], stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+    stdout, stderr = out.communicate()
+    stdout = stdout.lower()
+    
+    if 'dark' in stdout:
+        return 'Dark'
+    elif 'highcontrast' in stdout:
+        return 'HighContrast'
     else:
-        if theme.endswith('-dark'):
-            return 'Dark'
-        else:
-            return 'Light'
-
+        return 'Light'     
+    
 def isDark():
     return theme() == 'Dark'
 
