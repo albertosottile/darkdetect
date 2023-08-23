c215bf09c22f71ee565f95ceb0772a68f41eee72
raghavdhingra24 <106472154+raghavdhingra24@users.noreply.github.com>
2023-08-23 21:51:18 +0530

    Update _linux_detect.py to add listerner and code quality changes
    Removed some pass statements and added listener support while removing old one as it supports the old method also.

--- darkdetect/_linux_detect.py
+++ darkdetect/_linux_detect.py
@@ -11,15 +11,13 @@ def theme():
         #Using dbus-send command to get the theme of user using the freedesktop standards
         command = ["dbus-send","--session","--print-reply=literal","--dest=org.freedesktop.portal.Desktop", "/org/freedesktop/portal/desktop", "org.freedesktop.portal.Settings.Read", "string:org.freedesktop.appearance", "string:color-scheme"]
         out = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-        stdout=str(out.stdout.decode())
-        if len(stdout.strip())<1:
-            pass
-        else:
-            finalResult=stdout.replace("uint32","").strip("variant \n")
+        stdout = str(out.stdout.decode())
+        if len(stdout.strip())>1:
+            finalResult = stdout.replace("uint32","").strip("variant \n")
             #0=Default, 1=prefers-dark, 2=prefers-light
-            if finalResult=="1":
+            if finalResult == "1":
                 return 'Dark'
-            elif (finalResult=="2"):
+            elif finalResult == "2":
                 return 'Light'
             else:
                 #If result Default(0) or invalid, fallback to gtk-theme method
@@ -45,13 +43,52 @@ def isDark():
 
 def isLight():
     return theme() == 'Light'
-
 # def listener(callback: typing.Callable[[str], None]) -> None:
 def listener(callback):
+    _last_msg = ""
+    _last_theme = ""
+    command = ("dbus-monitor","--session","--monitor","sender=org.freedesktop.portal.Desktop, member=SettingChanged")
     with subprocess.Popen(
-        ('gsettings', 'monitor', 'org.gnome.desktop.interface', 'gtk-theme'),
+        command,
         stdout=subprocess.PIPE,
         universal_newlines=True,
     ) as p:
         for line in p.stdout:
-            callback('Dark' if '-dark' in line.strip().removeprefix("gtk-theme: '").removesuffix("'").lower() else 'Light')
+            #Freedesktop check
+            if "color-scheme" in _last_msg:
+                if "string" in line.strip():
+                    finalResult = line.strip("variant \n string \"")
+                    if finalResult == "prefer-dark":
+                        if _last_theme != "Dark":
+                            callback('Dark')
+                        _last_theme = "Dark"
+                    elif finalResult == "prefer-light":
+                        if _last_theme != "Light":
+                            callback('Light')
+                        _last_theme = "Light"
+                    else:
+                        if _last_theme != "Light":
+                            _last_theme = ""
+            #kde-theme check, required as there also after selecting light
+            #theme freedesktop spec is set to default
+            elif "colorscheme" in _last_msg:
+                if "string" in line.strip():
+                    finalResult = line.strip("variant \n string \"").lower()
+                    if 'dark' in finalResult and _last_theme != "Dark":
+                        callback('Dark')
+                        _last_theme = "Dark"
+                    elif _last_theme != "Light":
+                        callback('Light')
+                        _last_theme = "Light"
+            #gtk-theme check
+            elif "gtk-theme" in _last_msg:
+                if "string" in line.strip():
+                    finalResult = line.strip("variant \n string \"").lower()
+                    if '-dark' in finalResult:
+                        if _last_theme != "Dark":
+                            callback('Dark')
+                        _last_theme = "Dark"
+                    elif _last_theme != "Light":
+                        callback('Light')
+                        _last_theme = "Light"
+            _last_msg = line.strip().lower()
