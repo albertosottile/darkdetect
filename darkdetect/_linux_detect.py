ff685c760a11d7397ff822d051282368583b723e
raghavdhingra24 <106472154+raghavdhingra24@users.noreply.github.com>
2023-11-05 10:54:24 +0530

    Add fallback to gsettings method in case of dbus key not available in listener

--- darkdetect/_linux_detect.py
+++ darkdetect/_linux_detect.py
@@ -1,10 +1,20 @@
 #-----------------------------------------------------------------------------
-#  Copyright (C) 2019 Alberto Sottile, Eric Larson
+#  Copyright (C) 2023 Alberto Sottile, Eric Larson, Raghav Dhingra
 #
 #  Distributed under the terms of the 3-clause BSD License.
 #-----------------------------------------------------------------------------
 
 import subprocess
+
+def _check_dbus_support():
+    command = ["dbus-send","--session","--print-reply=literal","--dest=org.freedesktop.portal.Desktop", "/org/freedesktop/portal/desktop", "org.freedesktop.portal.Setttings.Read", "string:org.freedesktop.appearance", "string:color-scheme"]
+    out = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    #stdout = str(out.stdout.decode())
+    stderr = str(out.stderr.decode())
+    if "error" in stderr.lower():
+        return False
+    else:
+        return True
 
 def theme():
     try:
@@ -45,50 +55,59 @@ def isLight():
     return theme() == 'Light'
 # def listener(callback: typing.Callable[[str], None]) -> None:
 def listener(callback):
-    _last_msg = ""
-    _last_theme = ""
-    command = ("dbus-monitor","--session","--monitor","sender=org.freedesktop.portal.Desktop, member=SettingChanged")
-    with subprocess.Popen(
-        command,
-        stdout=subprocess.PIPE,
-        universal_newlines=True,
-    ) as p:
-        for line in p.stdout:
-            #Freedesktop check
-            if "color-scheme" in _last_msg:
-                if "string" in line.strip():
-                    finalResult = line.strip("variant \n string \"")
-                    if finalResult == "prefer-dark":
-                        if _last_theme != "Dark":
+    if _check_dbus_support():
+        _last_msg = ""
+        _last_theme = ""
+        command = ("dbus-monitor","--session","--monitor","sender=org.freedesktop.portal.Desktop, member=SettingChanged")
+        with subprocess.Popen(
+            command,
+            stdout=subprocess.PIPE,
+            universal_newlines=True,
+        ) as p:
+            for line in p.stdout:
+                #Freedesktop check
+                if "color-scheme" in _last_msg:
+                    if "string" in line.strip():
+                        finalResult = line.strip("variant \n string \"")
+                        if finalResult == "prefer-dark":
+                            if _last_theme != "Dark":
+                                callback('Dark')
+                            _last_theme = "Dark"
+                        elif finalResult == "prefer-light":
+                            if _last_theme != "Light":
+                                callback('Light')
+                            _last_theme = "Light"
+                        else:
+                            if _last_theme != "Light":
+                                _last_theme = ""
+                #kde-theme check, required as there also after selecting light
+                #theme freedesktop spec is set to default
+                elif "colorscheme" in _last_msg:
+                    if "string" in line.strip():
+                        finalResult = line.strip("variant \n string \"").lower()
+                        if 'dark' in finalResult and _last_theme != "Dark":
                             callback('Dark')
-                        _last_theme = "Dark"
-                    elif finalResult == "prefer-light":
-                        if _last_theme != "Light":
+                            _last_theme = "Dark"
+                        elif _last_theme != "Light":
                             callback('Light')
-                        _last_theme = "Light"
-                    else:
-                        if _last_theme != "Light":
-                            _last_theme = ""
-            #kde-theme check, required as there also after selecting light
-            #theme freedesktop spec is set to default
-            elif "colorscheme" in _last_msg:
-                if "string" in line.strip():
-                    finalResult = line.strip("variant \n string \"").lower()
-                    if 'dark' in finalResult and _last_theme != "Dark":
-                        callback('Dark')
-                        _last_theme = "Dark"
-                    elif _last_theme != "Light":
-                        callback('Light')
-                        _last_theme = "Light"
-            #gtk-theme check
-            elif "gtk-theme" in _last_msg:
-                if "string" in line.strip():
-                    finalResult = line.strip("variant \n string \"").lower()
-                    if '-dark' in finalResult:
-                        if _last_theme != "Dark":
-                            callback('Dark')
-                        _last_theme = "Dark"
-                    elif _last_theme != "Light":
-                        callback('Light')
-                        _last_theme = "Light"
-            _last_msg = line.strip().lower()
+                            _last_theme = "Light"
+                #gtk-theme check
+                elif "gtk-theme" in _last_msg:
+                    if "string" in line.strip():
+                        finalResult = line.strip("variant \n string \"").lower()
+                        if '-dark' in finalResult:
+                            if _last_theme != "Dark":
+                                callback('Dark')
+                            _last_theme = "Dark"
+                        elif _last_theme != "Light":
+                            callback('Light')
+                            _last_theme = "Light"
+                _last_msg = line.strip().lower()
+    else:
+        with subprocess.Popen(
+            ('gsettings', 'monitor', 'org.gnome.desktop.interface', 'gtk-theme'),
+            stdout=subprocess.PIPE,
+            universal_newlines=True,
+        ) as p:
+            for line in p.stdout:
+                callback('Dark' if '-dark' in line.strip().removeprefix("gtk-theme: '").removesuffix("'").lower() else 'Light')
